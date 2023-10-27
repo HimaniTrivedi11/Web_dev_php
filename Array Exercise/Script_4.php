f2ef318208491624c5434c1fccc87ff5c8c4d8a8
HimaniTrivedi11 <143804199+HimaniTrivedi11@users.noreply.github.com>
2023-10-27 15:53:31 +0530

    Check if the color "Green" exists in the array from question 1.

---
+++ Array Exercise/Script_4.php
@@ -0,0 +1,13 @@
+<?php
+
+$color=array("purple","green","aqua");
+
+if (in_array("green",$color))
+	{
+		echo "green color is available";
+	}
+else
+	{
+		echo "green color is not available";
+	}
+?>
